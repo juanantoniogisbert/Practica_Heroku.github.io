55764d72d35bc2ccf03e0c637962efd2ff1ab75e
Juan Antonio <juagisla@gmail.com>
2020-02-14 19:16:58 +0100

    added config varible de jAlejandro1

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+os.environ['DATABASE_HOST'] = 'ec2-54-246-89-234.eu-west-1.compute.amazonaws.com'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
